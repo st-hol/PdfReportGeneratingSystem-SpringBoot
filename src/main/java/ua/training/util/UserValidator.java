724db3a83923e44bdee27a9644619834ff393f90
Stanislav Holovachuk <Stanislav_Holovachuk@epam.com>
2019-11-12 16:10:22 +0200

    email pattern on validation

--- src/main/java/ua/training/util/UserValidator.java
+++ src/main/java/ua/training/util/UserValidator.java
@@ -1,5 +1,7 @@
 package ua.training.util;
 
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,10 @@ import ua.training.services.UserService;
 
 @Component
 public class UserValidator implements Validator {
+
+    private static final Pattern EMAIL_REGEX =
+            Pattern.compile("^[\\w\\d._-]+@[\\w\\d.-]+\\.[\\w\\d]{2,6}$");
+
 
     @Autowired
     @Qualifier("userServiceImpl")
@@ -28,6 +34,9 @@ public class UserValidator implements Validator {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
         if (user.getUsername().length() < 2 || user.getUsername().length() > 32) {
             errors.rejectValue("username", "Size.userForm.username");
+        }
+        if (user.getUsername() != null && ! EMAIL_REGEX.matcher(user.getUsername()).matches()) {
+            errors.rejectValue("username", "Username.email.invalid");
         }
         if (userService.findByUsername(user.getUsername()) != null) {
             errors.rejectValue("username", "Duplicate.userForm.username");
